196d8f0e59fe2f1caa9ca5f00383cea01cc2e8dc
emanuelplatypus <manoel.santos@sga.pucminas.br>
2020-12-03 13:45:29 -0300

    Add files via upload

---
+++ pages/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import Web3 from 'web3'
+import RegisterFactory from '../abis/RegisterFactory.json'
+import Navbar from './Navbar'
+import Main from './Main'
+
+class App extends Component {
+
+  async componentDidMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = RegisterFactory.networks[networkId]
+    if(networkData) {
+      const ReggisterFacttory = new web3.eth.Contract(RegisterFactory.abi, networkData.address)
+      this.setState({ ReggisterFacttory })
+      const contagemDeOcorrs = await ReggisterFacttory.methods.contagemDeOcorrs().call()
+      this.setState({ contagemDeOcorrs })
+      // Load tasks
+      for (var i = 1; i <= contagemDeOcorrs; i++) {
+        const tareffa = await ReggisterFacttory.methods.tasks(i).call()
+        this.setState({
+          tasks: [...this.state.tasks, tareffa]
+        })
+      }
+      this.setState({ loading: false})
+    } else {
+      window.alert('RegisterFactory contract not deployed to detected network.')
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      contagemDeOcorrs: 0,
+      tasks: [],
+      loading: true
+    }
+	
+
+    this.createTask = this.createTask.bind(this)
+  }
+
+  createTask(content, numReeggistro) {
+    this.setState({ loading: true })
+    this.state.ReggisterFacttory.methods.createTask(content, numReeggistro).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex">
+              { this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                : <Main
+                  tasks={this.state.tasks}
+                  createTask={this.createTask}
+                  />
+              }			  
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ pages/Main.js
@@ -0,0 +1,176 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class Main extends Component {
+	
+	constructor(props) {
+    super(props);
+		this.state = { username: ''};
+		  }
+	
+	myChangeHandler = (event) => {
+		  const opcao1 = this.tippoUssuario.value
+		  const nome1 = this.nomeAssociado.value		  
+		  const placa1 = this.tareffaPlacca.value
+		  const datta1 = this.tareffaData.value
+		  const ocorr1 = this.tareffaOcorrencia.value	  	  
+		  var anox = datta1.substring(0, 4);
+		  var mesex = datta1.substring(5, 7);
+		  var dia = datta1.substring(8, 10);
+		  var dataFormatada = dia + '/'+ mesex +'/'+ anox;
+		  this.setState({
+			usercontent:
+		    opcao1+'; '+nome1+'; '+dataFormatada+'; '+placa1+'; '+ocorr1+'; '
+		  });
+	}
+	
+  	onChange(e){
+		
+		const crypto = require('crypto');
+		var hashedCode  = '';
+		let filless = e.target.files;
+		let reader = new FileReader();
+		reader.readAsDataURL(filless[0]);
+		reader.onload = (e) => {
+			hashedCode  = crypto.createHash('sha1').update(e.target.result).digest('hex');
+			document.getElementById('tareffaHashDocumento').value = hashedCode;
+		}
+	}
+	
+	   
+  render() {
+    return (
+	
+      <div id="content">
+        <h2>Adicionar registro</h2>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+		  const opcao1 = this.tippoUssuario.value
+          const nome1 = this.nomeAssociado.value		  
+		  const placa1 = this.tareffaPlacca.value
+		  const datta1 = this.tareffaData.value
+		  const ocorr1 = this.tareffaOcorrencia.value		  
+		  const hashd1 = this.tareffaHashDocumento.value
+		  var anox = datta1.substring(0, 4);
+		  var mesex = datta1.substring(5, 7);
+		  var dia = datta1.substring(8, 10);
+		  var dataFormatada = dia + '/'+ mesex +'/'+ anox;
+		  var timestamp = new Date().getTime();
+          this.props.createTask(opcao1+' '+nome1+' '+dataFormatada+' '+placa1+' '+ocorr1+' '+hashd1, timestamp)
+        }}>
+		
+		  <div className="form-group mr-sm-2">	
+		  
+		  <label for="cars"><span style={{color: '#007bff'}}>Usuário:&nbsp;&nbsp;&nbsp;</span></label>
+			<select id="tippoUssuario" content="cars" onChange={this.myChangeHandler} ref={(select) => { this.tippoUssuario = select }} required>   
+			  <option value="">Selecione abaixo a categoria</option>
+			  <option value="1–Associado">1–Associado</option>
+			  <option value="2–Operador">2–Operador</option>
+		   </select>
+		    
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="nomeAssociado"
+              type="text"
+			  onChange={this.myChangeHandler}
+              ref={(input) => { this.nomeAssociado = input }}
+              className="form-control"
+              placeholder="Nome do associado"
+              required />			
+          </div>
+          
+		  <div className="form-group mr-sm-2">
+            <input
+              id="tareffaData"
+              type="date"
+			  onChange={this.myChangeHandler}
+              ref={(input) => { this.tareffaData = input }}
+              className="form-control"
+              max={moment().format("YYYY-MM-DD")}
+              required />
+          </div>
+		  
+		  <div className="form-group mr-sm-2">
+            <input
+              id="tareffaPlacca"
+              type="text"
+			  onChange={this.myChangeHandler}
+              ref={(input) => { this.tareffaPlacca = input }}
+              className="form-control"
+              placeholder="Placa"
+              required />
+          </div>
+		  
+		  <div className="form-group mr-sm-2">
+            <input
+              id="tareffaOcorrencia"
+              type="text"
+			  onChange={this.myChangeHandler}
+              ref={(input) => { this.tareffaOcorrencia = input }}
+              className="form-control"
+              placeholder="Ocorrencia"
+              required />
+          </div>
+		  
+		  		 
+		<div className="form-group mr-sm-2">
+		  <input type='text' class="form-control" placeholder="Registro completo" value={this.state.usercontent} />		
+		</div>
+		  
+		  <div className="form-group mr-sm-2">
+            <input
+              id="tareffaHashDocumento"
+              type="text"
+			  onChange={this.myChangeHandler}
+              ref={(input) => { this.tareffaHashDocumento = input }}
+              className="form-control"
+              placeholder="Código do documento"
+              /*required*/ />
+          </div>
+
+		<p></p>
+		
+		  <div>		
+			  <div onSubmit={this.onFormSubmit}>
+			  <h6>Se desejar anexar hash (código) de documento, siga as intruções abaixo.</h6>
+			  <input type='file' content='file' onChange={(e)=>this.onChange(e)} />
+			  </div>   
+        </div>
+		<p></p>
+		  
+          <button type="submit" className="btn btn-primary">Enviar dados</button>
+        </form>
+		
+
+        <p>&nbsp;</p>
+        <h6>Ocorrências</h6>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+			  <th scope="col">Núm do Registro</th>
+              <th scope="col">Ocorrência</th>              
+              <th scope="col">Endereço</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody id="tareffaList">
+            { this.props.tasks.map((tareffa, key) => {
+              return(
+                <tr key={key}>
+                  <th scope="row">{tareffa.id.toString()}</th>
+				  <td>{tareffa.numReeggistro.toString()}</td>
+                  <td>{tareffa.content}</td>                  
+                  <td>{tareffa.owner}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Main;
